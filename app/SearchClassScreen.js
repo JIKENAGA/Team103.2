40da596490b42e7efb72c64be0366cf23f6345a8
Phayden2 <parkerhayden1119@gmail.com>
2023-04-11 20:54:48 -0400

    Search Screen
    Got the search screen to query the database with the inputted text, and display the results in individual boxes with the name of the course in them.

--- app/SearchClassScreen.js
+++ app/SearchClassScreen.js
@@ -1,14 +1,11 @@
-import React, { useState, useEffect, FlatList } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, } from 'react-native';
-import 'firebase/firestore';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 import { db } from './Firebase/firebase';
 import { getDatabase, ref, query, orderByChild, startAt, endAt, onValue } from 'firebase/database';
-
 
 export default function SearchScreen() {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
-
 
   const handleSearch = () => {
     console.log(`Searching for "${searchTerm}"...`);
@@ -16,10 +13,8 @@ export default function SearchScreen() {
     const queryRef = query(
       dataRef,
       orderByChild('Short Title'),
-      startAt(searchTerm.toLowerCase()),
       endAt(searchTerm.toLowerCase() + '\uf8ff')
     );
-    console.log(queryRef)
     const results = [];
 
     onValue(queryRef, (snapshot) => {
@@ -29,70 +24,185 @@ export default function SearchScreen() {
           results.push(data);
         }
       });
-      console.log(results)
+      console.log(results);
       setSearchResults(results);
     });
-    console.log(searchResults)
   };
 
-  const renderSearchResults = ({ item }) => {
+  const renderSearchResult = ({ item }) => {
     return (
-      <View>
-        <Text>{item.name}</Text>
-      </View>
+      <TouchableOpacity style={styles.result} onPress={() => console.log(`Pressed ${item['Short Title']}`)}>
+        <Text style={styles.resultText}>{item['Short Title']}</Text>
+      </TouchableOpacity>
     );
   };
 
-  
-
   return (
     <View style={styles.container}>
-    <Text style={styles.title}>Search Classes</Text>
+      <Text style={styles.title}>Search Classes</Text>
 
-    <TextInput
-      style={styles.input}
-      placeholder="Enter search term"
-      value={searchTerm}
-      onChangeText={(text) => setSearchTerm(text)}
-    />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter search term"
+        value={searchTerm}
+        onChangeText={(text) => setSearchTerm(text)}
+      />
 
-    <TouchableOpacity
-      style={styles.button}
-      onPress={handleSearch}
-    >
-      <Text style={styles.buttonText}>Search</Text>
-    </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleSearch}>
+        <Text style={styles.buttonText}>Search</Text>
+      </TouchableOpacity>
 
+      <FlatList
+        data={searchResults}
+        renderItem={renderSearchResult}
+        keyExtractor={(item) => item['ID']}
+        style={styles.resultsList}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  padding: 20,
-},
-title: {
-  fontSize: 20,
-  fontWeight: 'bold',
-  marginTop: 30,
-  marginBottom: 20,
-},
-input: {
-  height: 40,
-  borderColor: 'gray',
-  borderWidth: 1,
-  marginBottom: 10,
-},
-button: {
-  backgroundColor: 'gray',
-  padding: 10,
-  borderRadius: 5,
-},
-buttonText: {
-  color: 'white',
-  textAlign: 'center',
-},
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: 'gray',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  resultsList: {
+    marginTop: 20,
+  },
+  result: {
+    backgroundColor: 'lightgray',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  resultText: {
+    fontSize: 16,
+  },
 });
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect, FlatList } from 'react';
+// import { View, Text, TextInput, TouchableOpacity, StyleSheet, } from 'react-native';
+// import 'firebase/firestore';
+// import { db } from './Firebase/firebase';
+// import { getDatabase, ref, query, orderByChild, startAt, endAt, onValue, } from 'firebase/database';
+
+
+// export default function SearchScreen() {
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const [searchResults, setSearchResults] = useState([]);
+
+
+//   const handleSearch = () => {
+//     console.log(`Searching for "${searchTerm}"...`);
+//     const dataRef = ref(db, 'classes');
+//     const queryRef = query(
+//       dataRef,
+//       orderByChild('Short Title'),
+//       endAt(searchTerm.toLowerCase() + '\uf8ff')
+//     );
+//     //console.log(queryRef)
+//     const results = [];
+  
+//     onValue(queryRef, (snapshot) => {
+//       snapshot.forEach((child) => {
+//         const data = child.val();
+//         if (data['Short Title'].toLowerCase().includes(searchTerm.toLowerCase())) {
+//           results.push(data);
+//         }
+//       });
+//       console.log(results);
+//       setSearchResults(results);
+//     });
+//   };
+
+//   const renderSearchResults = ({ item }) => {
+//     return (
+//       <View>
+//         <Text>{item}</Text>
+//       </View>
+//     );
+//   };
+
+  
+
+//   return (
+//     <View style={styles.container}>
+//     <Text style={styles.title}>Search Classes</Text>
+
+//     <TextInput
+//       style={styles.input}
+//       placeholder="Enter search term"
+//       value={searchTerm}
+//       onChangeText={(text) => setSearchTerm(text)}
+//     />
+
+//     <TouchableOpacity
+//       style={styles.button}
+//       onPress={handleSearch}
+//     >
+//       <Text style={styles.buttonText}>Search</Text>
+//     </TouchableOpacity>
+
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+// container: {
+//   flex: 1,
+//   padding: 20,
+// },
+// title: {
+//   fontSize: 20,
+//   fontWeight: 'bold',
+//   marginTop: 30,
+//   marginBottom: 20,
+// },
+// input: {
+//   height: 40,
+//   borderColor: 'gray',
+//   borderWidth: 1,
+//   marginBottom: 10,
+// },
+// button: {
+//   backgroundColor: 'gray',
+//   padding: 10,
+//   borderRadius: 5,
+// },
+// buttonText: {
+//   color: 'white',
+//   textAlign: 'center',
+// },
+// });
 
 
